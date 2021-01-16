e15278e4661bf27cb700a1b581421639c66ebd8d
Katherine Alexandra Florez Ramirez <katherine.florez12@gmail.com>
2021-01-16 18:21:38 -0500

    Create ReadFile - reads .txt file and save the matrix

--- Methods.cpp
+++ Methods.cpp
@@ -1,1 +1,51 @@
 #include "Methods.h"
+#include <fstream>      
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+using std::vector;
+
+vector<vector<int> > Methods::ReadFile(const string name)
+{
+    vector< vector<int> > matrix;
+    ifstream infile(name);
+
+    if (!infile)
+    {
+        cout << "Unable to open file " << name << endl;
+    }
+    else
+    {
+        int m;
+        int n;
+        infile >> m >> n;
+
+        if (m > 0 && n > 0)
+        {
+            cout << "Matrix dimensions " << m << "x" << n << endl;
+            int data;
+
+            for (int i = 0; i < m; ++i)
+            {
+                vector<int> row;
+
+                for (int j = 0; j < n; ++j)
+                {
+                    infile >> data;
+                    row.push_back(data);
+                }
+
+                matrix.push_back(row);
+            }
+        }
+        else
+        {
+            cout << "At least one of the dimensions is zero  " << m << "x" << n << endl;
+        }
+    }
+
+    return matrix;
+}
--- Methods.h
+++ Methods.h
@@ -1,11 +1,15 @@
 #pragma once
 // Methods class contains the methods for calcule the longest decreasing path 
+#include<string>
+#include<vector>
 
 class Methods final
 {
 	public:
 		Methods() = default;
 		~Methods() = default;
+
+		std::vector<std::vector<int> > ReadFile(const std::string name);
 
 	private:
 };
