88d9d1f11e091161b3f468c0f18e7c419bf75b99
Katherine Alexandra Florez Ramirez <katherine.florez12@gmail.com>
2021-01-16 18:28:21 -0500

    Create structure with maxDepth, maxLength and maxPath information

--- InfoLongestPath.h
+++ InfoLongestPath.h
@@ -1,7 +1,11 @@
 #pragma once
-// InfoLongestPath.h : header file contain the structure InfoLongestPath 
+// InfoLongestPath.h : header file contain the structure InfoLongestPath
+
+#include<vector>
 
 struct InfoLongestPath
 {
-	
+	int maxDepth = 0;
+	int maxLength = 0;
+	std::vector<int> maxPath;
 };
