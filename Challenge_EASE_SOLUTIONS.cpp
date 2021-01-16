7ff3d728b9aaa7ca99de144356223f0ed29ee557
Katherine Alexandra Florez Ramirez <katherine.florez12@gmail.com>
2021-01-16 18:23:52 -0500

    get file name by console, and read file with ReadFile method

--- Challenge_EASE_SOLUTIONS.cpp
+++ Challenge_EASE_SOLUTIONS.cpp
@@ -1,8 +1,20 @@
 // Challenge_EASE_SOLUTIONS.cpp : main file 
-
+#include "Methods.h"      
 #include <iostream>
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
 
 int main()
 {
+    string fileName;
+    cout << "Enter file location: ";
+    cin >> fileName;
+
+    Methods methods;
+    vector< vector<int> > matrix = methods.ReadFile(fileName);
+
     return 0;
 }
