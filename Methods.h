6eb817a8715eac3c59e9159d3c7e8bf164345e70
Katherine Alexandra Florez Ramirez <katherine.florez12@gmail.com>
2021-01-16 18:00:43 -0500

    Create Methods.cpp and Methods.h - class contains the methods for calcule the longest decreasing path

---
+++ Methods.h
@@ -0,0 +1,12 @@
+#pragma once
+// Methods class contains the methods for calcule the longest decreasing path 
+
+class Methods final
+{
+	public:
+		Methods() = default;
+		~Methods() = default;
+
+	private:
+};
+
