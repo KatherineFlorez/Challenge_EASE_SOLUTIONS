ff15957d3a80c2f8f3b32d9c12f90d3e69206045
Katherine Alexandra Florez Ramirez <katherine.florez12@gmail.com>
2021-01-16 18:51:27 -0500

    include description of methods

--- Methods.h
+++ Methods.h
@@ -9,12 +9,14 @@ class Methods final
 		Methods() = default;
 		~Methods() = default;
 
+		//Read .txt file and save the information
 		std::vector<std::vector<int> > ReadFile(const std::string name);
+		//Main method to calculate Longesth Path
 		InfoLongestPath CalculateLongestPath(const std::vector<std::vector<int> > matrix);
 
 	private:
+		//Depth First Search algorithm for a decreasing path
 		int DFS(int i, int j, const std::vector<std::vector<int> >& matrix, std::vector<std::vector<int> >& length);
-		InfoLongestPath GetPathInfo(int i, int j, const std::vector<std::vector<int>>& matrix, const std::vector<std::vector<int>>& length);
-
+		
 };
 
